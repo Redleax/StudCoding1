3a78ca48bdd65f7110a1744f0ed80e8d16251ec4
Redleax <MrZlooba@yandex.ru>
2022-12-19 16:48:56 +0300

    Praktika 8 create

---
+++ ConsoleApplication2/Praktika_8.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#include "Praktika_8.h"
+#include "myfunc.h"
+
+#define arrayLength 50
+#define randMin -100
+#define randMax 100
+
+void Task_8_1();
+
+void Praktika_8() {
+	int taskNumber = -1;
+
+	while (taskNumber != 0) {
+		printf("Enter the task number (0 to exit) => ");
+		scanf_s("%i", &taskNumber);
+		switch (taskNumber) {
+		case 1:
+			Task_8_1();
+			break;
+		}
+	}
+}
+
+void Task_8_1() {
+
+}
